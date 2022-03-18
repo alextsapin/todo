151382b1e422a780621a31f24beacdb7ff978cce
alextsapin <alextsapin@yandex.ru>
2022-03-18 16:45:50 +0200

    Edit app

---
+++ src/components/FullInput/FullInput.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {v1} from 'uuid';
+
+const FullInput = () => {
+
+    return (
+        <div>
+            <input />
+        </div>
+    )
+}
+
+export default FullInput;
