f867dc9017fc4fa1c4794a7d9ccc402ce1f4846f
alextsapin <alextsapin@yandex.ru>
2022-06-26 22:07:43 +0200

    Completed filter test

--- src/redux/reducers/todo.test.ts
+++ src/redux/reducers/todo.test.ts
@@ -1,6 +1,8 @@
 import todoReducer from './todo';
 import {v1} from 'uuid';
 
+import {deleteTodoListAC, addTodoListAC, changeTodoListTitleAC, changeTodoListFilterAC} from './todo';
+
 test('Correct todolist should be deleted', () => {
    const id1 = v1();
    const id2 = v1();
@@ -10,7 +12,7 @@ test('Correct todolist should be deleted', () => {
        {id: id2, title: "What to buy", filter: "all"}
    ]
 
-   const endState = todoReducer(startState, {type: 'DELETE_TODO_LIST', id: id1})
+   const endState = todoReducer(startState, deleteTodoListAC(id1))
 
    expect(endState.length).toBe(1);
    expect(endState[0].id).toBe(id2);
@@ -28,8 +30,41 @@ test('Correct todolist should be added', () => {
         {id: id2, title: "What to buy", filter: "all"}
     ]
  
-    const endState = todoReducer(startState, {type: 'ADD_TODO_LIST', title: title})
+    const endState = todoReducer(startState, addTodoListAC(title))
  
     expect(endState.length).toBe(3);
     expect(endState[2].title).toBe(title);
+ });
+
+ test('todoList title should be changed', () => {
+    const id1 = v1();
+    const id2 = v1();
+
+    const title = 'New title';
+
+
+    const startState = [
+        {id: id1, title: "What to learn", filter: "all"},
+        {id: id2, title: "What to buy", filter: "all"}
+    ]
+ 
+    const endState = todoReducer(startState, changeTodoListTitleAC(id1, title))
+ 
+    expect(endState.length).toBe(2);
+    expect(endState[0].title).toBe(title);
+ });
+
+ test('todoList filter should be changed', () => {
+    const id1 = v1();
+    const id2 = v1();
+
+    const startState = [
+        {id: id1, title: "What to learn", filter: "all"},
+        {id: id2, title: "What to buy", filter: "all"}
+    ]
+ 
+    const endState = todoReducer(startState, changeTodoListFilterAC(id1, 'ALL'))
+ 
+    expect(endState.length).toBe(2);
+    expect(endState[0].filter).toBe('ALL');
  });
--- src/redux/reducers/todo.ts
+++ src/redux/reducers/todo.ts
@@ -10,12 +10,16 @@ type todoListType = {
     filter: string
 }
 
+export type filterType = 'ALL' | 'ACTIVE' | 'COMPLETED';
+
 export type stateType = Array<todoListType>
 
 const initialState = [
     {id: todoListID1, title: 'What to learn:', filter: 'all'},
     {id: todoListID2, title: 'What to buy:', filter: 'all'},
 ]
+
+type ACType = deleteTodoListType | addTodoListType | changeTodoListTitleType | changeTodoListFilterType;
 
 const todoReducer = (state = initialState, action: ACType)   => {
     switch(action.type) {
@@ -32,6 +36,14 @@ const todoReducer = (state = initialState, action: ACType)   => {
         case 'DELETE_TODO_LIST': {
             return [...state].filter(item => item.id !== action.id)
         }
+
+        case 'CHANGE_TODO_LIST_TITLE': {
+            return [...state].map(item => item.id === action.id ? {...item, title: action.title} : item)
+        }
+
+        case 'CHANGE_TODO_LIST_FILTER': {
+            return [...state].map(item => item.id === action.id ? {...item, filter: action.filter} : item)
+        }
         
         default: {
             return state
@@ -44,9 +56,9 @@ type deleteTodoListType = {
     id: string
 }
 
-const deleteTodoListAC = (id: string) => {
+export const deleteTodoListAC = (id: string) => {
     return {
-        type: 'DELETE_TODO_LIST',
+        type: 'DELETE_TODO_LIST' as const,
         id
     }
 }
@@ -56,12 +68,38 @@ type addTodoListType = {
     title: string
 }
 
-type ACType = deleteTodoListType | addTodoListType
-
-const addTodoListAC = (title: string) => {
+export const addTodoListAC = (title: string) => {
     return {
-        type: 'ADD_TODO_LIST',
+        type: 'ADD_TODO_LIST' as const,
         title
+    }
+}
+
+type changeTodoListTitleType = {
+    type: 'CHANGE_TODO_LIST_TITLE',
+    id: string
+    title: string
+}
+
+export const changeTodoListTitleAC = (id: string, title: string) => {
+    return {
+        type: 'CHANGE_TODO_LIST_TITLE' as const,
+        id,
+        title
+    }
+}
+
+type changeTodoListFilterType = {
+    type: 'CHANGE_TODO_LIST_FILTER',
+    id: string
+    filter: filterType
+}
+
+export const changeTodoListFilterAC = (id: string, filter: filterType) => {
+    return {
+        type: 'CHANGE_TODO_LIST_FILTER' as const,
+        id,
+        filter
     }
 }
 
